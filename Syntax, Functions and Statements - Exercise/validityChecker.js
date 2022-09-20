daf96fb165e5ef50de2cdbd5f6da161756871ca2
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-20 14:05:00 +0300

    problem solving with functions;

--- Syntax, Functions and Statements - Exercise/validityChecker.js
+++ Syntax, Functions and Statements - Exercise/validityChecker.js
@@ -1,14 +1,23 @@
 function validityChecker(x1, y1, x2, y2) {
-    let distansValidation;
-    let firstPoint = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
-    firstPoint === Math.floor(firstPoint) ? distansValidation = 'valid' : distansValidation = 'invalid';
-    console.log(`{${x1}, ${y1}} to {0, 0} is ${distansValidation}`);
-    let secondPoint = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
-    secondPoint === Math.floor(secondPoint) ? distansValidation = 'valid' : distansValidation = 'invalid';
-    console.log(`{${x2}, ${y2}} to {0, 0} is ${distansValidation}`);
-    let distans = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    distans === Math.floor(distans) ? distansValidation = 'valid' : distansValidation = 'invalid';
-    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${distansValidation}`);
+    let firstPoint = calculate(x1, y1, 0, 0);
+    let secondPoint = calculate(x2, y2, 0, 0);
+    let distans = calculate(x1, y1, x2, y2);
+    let firstValidation = validation(firstPoint);
+    let secondValidation = validation(secondPoint);
+    let distansValidation = validation(distans);
+    print(x1, y1, 0, 0, firstValidation);
+    print(x2, y2, 0, 0, secondValidation);
+    print(x1, y1, x2, y2, distansValidation);
+
+    function calculate(x1, y1, x2, y2) {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+    function validation(point) {
+        return point === Math.floor(point) ? 'valid' : 'invalid';
+    }
+    function print(x1, y1, x2, y2, valid) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${valid}`);
+    }
 }
 validityChecker(3, 0, 0, 4);
 validityChecker(2, 1, 1, 1)
