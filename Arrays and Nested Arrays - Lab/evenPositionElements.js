992ec93f6a64416b925ced259dafdab524517353
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-20 16:05:07 +0300

    problem solving- filter notes

--- Arrays and Nested Arrays - Lab/evenPositionElements.js
+++ Arrays and Nested Arrays - Lab/evenPositionElements.js
@@ -1,7 +1,8 @@
 function evenPositionElements(arr) {
-    let res = arr.filter((x, index) =>
-        index % 2 === 0).join(' ');
-    console.log(res)
+    let res = arr.filter((x, index) =>  //по този начин достъпвам индекса на масива
+        index % 2 === 0)                // и манипулирам чрез него
+        .join(' ');
+    console.log(res);
 }
 evenPositionElements(['20', '30', '40', '50', '60']);
 evenPositionElements(['5', '10'])
