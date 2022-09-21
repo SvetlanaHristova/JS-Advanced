4cd0da987c873abd8d3b19306b70b4c6b0e840aa
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-21 11:57:39 +0300

    problem solving

---
+++ Arrays and Nested Arrays - Lab/pieceOfPie.js
@@ -0,0 +1,20 @@
+function pieceOfPie(arr,pieStart,pieEnd) {
+
+   return arr.slice(arr.indexOf(pieStart),arr.indexOf(pieEnd)+1);
+   
+}
+console.log(pieceOfPie(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'));
+console.log(pieceOfPie(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Apple Crisp',
+'Pot Pie'))
