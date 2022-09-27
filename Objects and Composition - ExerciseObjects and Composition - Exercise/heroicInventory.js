b54beeb3c6cda7195e9249c1421950c967d2161c
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-27 12:27:33 +0300

    https://judge.softuni.org/Contests/Compete/Index/2759#3

--- Objects and Composition - ExerciseObjects and Composition - Exercise/heroicInventory.js
+++ Objects and Composition - ExerciseObjects and Composition - Exercise/heroicInventory.js
@@ -1,14 +1,17 @@
 function heroicInventory(data) {
-    let res = []
-    data.map(x => x.split(' / ')).map(el => {
-        let obj = {};
-        obj['name'] = el[0];
-        obj['level'] = Number(el[1]);
-        let items=el[2] ?  el[2].split(', ') : [];
-        obj['items'] = items;
-        res.push(obj);
-    });
-    return JSON.stringify(res)
+    let res = [];
+    
+    data.map(x => x.split(' / '))
+        .map(el => {
+            let obj = {};
+            obj['name'] = el[0];
+            obj['level'] = Number(el[1]);
+            let items = el[2] ? el[2].split(', ') : [];
+            obj['items'] = items;
+            res.push(obj);
+        });
+
+    return JSON.stringify(res);
 }
 console.log(heroicInventory(['Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
