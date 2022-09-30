5c444d4ccd2a13194a4ff24e8ff8cbfc72a99179
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-30 14:59:16 +0300

    https://judge.softuni.org/Contests/Compete/Index/2761#7  with notis

--- DOM Introduction - Exercise/08. Generate Report/report.js
+++ DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,6 +1,7 @@
 function generateReport() {
     let textResult = document.getElementById("output");
-    let heding = document.querySelectorAll("input");
+    let heding = document.querySelectorAll("input");       //връща NodeList(7) [ input, ..., input ] можем да достъпим  name или type от инпута
+                                                            //ако е ("td"или друг таг)NodeList(140) [ td, ... td,]
     let information = document.querySelectorAll("tbody tr");
 
     let result = [];
@@ -9,15 +10,13 @@ function generateReport() {
 
         for (let j = 0; j < heding.length; j++) {
             if (heding[j].checked) {   //това е при checkbox да е маркирано
-                obj[heding[j].name] = information[i].children[j].textContent
-
+                obj[heding[j].name] = information[i].children[j].textContent;
+                                // тук ползвам името
             }
 
         }
         result.push(obj)
     }
-
-
     textResult.textContent = JSON.stringify(result)
 }
 
