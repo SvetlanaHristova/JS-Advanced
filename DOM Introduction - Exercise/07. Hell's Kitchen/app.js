21e7f502aa2e9b1b63682a47369d615d7a58b13a
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-30 11:05:36 +0300

    https://judge.softuni.org/Contests/Compete/Index/2761#6

--- DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -7,33 +7,37 @@ function solve() {
 
       for (let element of input) {
          let [restaurantName, workersList] = element.split(' - ');
-         if (!result.find(x => x.name = restaurantName)) {
-            let obj = {
+         if (!result.find(x => x.name === restaurantName)) {
+            result.push({
                name: restaurantName,
                totalSalary: 0,
                countWorkars: 0,
                personal: {}
-            }
+            });
          }
+         let curentRestaurant = result.find(x => x['name'] === restaurantName);
          let personalInfo = workersList.split(', ')
          for (let el of personalInfo) {
             let [worker, salary] = el.split(' ');
-            obj.personal[`${worker}`] = Number(salary);
-            obj.totalSalary += Number(salary);
-            obj.countWorkars++;
+            curentRestaurant.personal[`${worker}`] = Number(salary);
+            curentRestaurant.totalSalary += Number(salary);
+            curentRestaurant.countWorkars++;
          }
-         result.push(obj);
       }
-
       let best = result.sort((a, b) => {
          return b.totalSalary / b.countWorkars - a.totalSalary / a.countWorkars
       })[0];
       let bestRestaurant = best.name;
-      let averageSalary = best.totalSalary / best.countWorkars;
-      let personalArr = Object.from.entries(best.personal);
-   }
-}
-debugger;
-//["PizzaHut - Peter 500, George 300, Mark 800",
-//"TheLake - Bob 1300, Joe 780, Jane 660"]
+      let averageSalary = (best.totalSalary / best.countWorkars).toFixed(2);
+      let personalArr = Object.entries(best.personal);
+      personalArr.sort(([aA, aB], [bA, bB]) => { return bB - aB });
+      let textResultat = "";
+      let bestSalary = personalArr[0][1];
+      for (let arr of personalArr) {
+         textResultat += `Name: ${arr[0]} With Salary: ${arr[1]} `
+      }
+      document.querySelector("#bestRestaurant p").textContent = `Name: ${bestRestaurant} Average Salary: ${averageSalary} Best Salary: ${bestSalary.toFixed(2)}`;
+      document.querySelector("#workers p").textContent = textResultat;
+      }
 
+}
