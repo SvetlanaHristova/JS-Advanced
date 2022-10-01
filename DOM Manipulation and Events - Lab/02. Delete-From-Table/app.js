d824c60af8019d9e0821142ff2ab5f085f6e4b25
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-10-01 15:44:46 +0300

    https://judge.softuni.org/Contests/Practice/Index/2762#1

--- DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,11 +1,11 @@
 function deleteByEmail() {
     let inputElement = document.querySelector("input[type=text]");
     let columnElements = document.querySelectorAll("tbody tr td:nth-child(2)");
-    let resultElement = document.querySelector("#result")
+    let resultElement = document.querySelector("#result");
     for (let email of columnElements) {
         if (email.textContent === inputElement.value) {
             email.parentElement.remove();
-            resultElement.textContent = "Deleted."
+            resultElement.textContent = "Deleted.";
         }else{
             resultElement.textContent ="Not found."
         }
