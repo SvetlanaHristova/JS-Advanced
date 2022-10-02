fa8ad670c10158d723bc5f22ec5744adf94092a7
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-10-02 10:35:12 +0300

    https://judge.softuni.org/Contests/Practice/Index/2762#4

---
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -0,0 +1,14 @@
+function focused() {
+    let divElements = document.querySelectorAll("input");
+    for (let divElement of divElements) {
+        divElement.addEventListener('focus', makeClass);
+        divElement.addEventListener('blur', deleteClass);
+    };
+    function makeClass(el) {
+        el.target.parentElement.classList.add("focused");
+    };
+    function deleteClass(el) {
+        el.target.parentElement.classList.remove("focused");
+    };
+    console.log(divElements);
+}
