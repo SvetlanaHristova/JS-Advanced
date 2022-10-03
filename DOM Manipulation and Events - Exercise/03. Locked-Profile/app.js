55f1232caba39254646d4b4eed3d260585712d0b
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-10-03 21:01:43 +0300

    https://judge.softuni.org/Contests/Compete/Index/2763#2

---
+++ DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -0,0 +1,17 @@
+function lockedProfile() {
+    let profils = document.querySelectorAll("div.profile");
+
+    for (let profile of profils) {
+        profile.getElementsByTagName("button")[0]
+            .addEventListener('click', shown);
+    };
+
+    function shown(elements) {
+        let currentProfil = elements.target.parentElement;
+        if (currentProfil.querySelector('input[type = "radio"][value = "unlock"]').checked) {
+            let hidenElement = currentProfil.querySelector("div");
+            hidenElement.style.display = "block";
+            currentProfil.lastElementChild.textContent = "Hide it";
+        };
+    };
+};
