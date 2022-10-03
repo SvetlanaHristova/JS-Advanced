97cc4d6bda5b3223f1db1b67d839e4069276fa04
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-10-03 17:02:56 +0300

    https://judge.softuni.org/Contests/Practice/Index/2762#6

--- DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,44 +1,33 @@
 function solve() {
    document.getElementsByClassName("shopping-cart")[0].addEventListener('click', addToList);
-
-
+   document.getElementsByClassName("checkout")[0].addEventListener('click', addResult);
+   let textarea = document.querySelector("div textarea");
+   let productInBag = new Set();
+   let totalPrice = 0;
+   textarea.textContent = "";
    function addToList(el) {
+
       if (el.target.tagName === "BUTTON" && el.target.className === "add-product") {
+         // console.log(el.target.tagName)   !!!достъпваме елемента който слушаме по таг
          let product = el.target.parentElement.parentElement;
-         let name=product.getElementsByClassName("product-title")[0].textContent;
-         let price=Number(product.getElementsByClassName("product-line-price")[0].textContent);
+         let name = product.getElementsByClassName("product-title")[0].textContent;
+         let price = Number(product.getElementsByClassName("product-line-price")[0].textContent);
+         productInBag.add(name);
+         totalPrice += price;
+         textarea.textContent += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+      };
+   };
+   function addResult(element) {
 
-         console.log(price)
-      }
-      // console.log(el.target.tagName)       !!!
+      list = Array.from(productInBag).join(', ');
+      textarea.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`;
+      
+      document.getElementsByClassName("shopping-cart")[0].removeEventListener('click', addToList);
+      document.getElementsByClassName("checkout")[0].removeEventListener('click', addResult);
 
-   }
-}
+   };
+};
 
-/*let addProducts = document.querySelectorAll("div.product-add");
-   let checkoutElement = document.querySelector("button.checkout");
 
-   for (let product of addProducts) {
-      product.addEventListener('click', addToBag)
-   }
 
-   function addToBag(e) {
-      let productName = e.target.cildren(2)
-      let productPrice = Number(e.currentTarget.lastElementChild)
-      let chopingList = document.querySelector("div textarea")
-      let infoProduct = document.createElement("label")
-      infoProduct.innerHTML = `Added ${productName} for ${productPrice} to the cart.\n`
-      chopingList.appendChild(infoProduct)
-   }*/
-/* let priceProduct=product.lastElementChild*//*let productsPropertyElements = document.querySelectorAll("div.product");
-   let mapProducts = new Map()
-   for (let product of productsPropertyElements) {
-     let productName=product.children[1].children[0].textContent;
-     let productPrice=productName.cildren[3]
-     let 
-      console.log(productName);
-   }
 
-   debugger
-
-   /*let product =*/
