cf0a833234c136d9c591410948f058482a07a702
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-10-03 17:38:22 +0300

    https://judge.softuni.org/Contests/Compete/Index/2763#0

---
+++ DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,22 @@
+function create(words) {
+   let parentElement = document.querySelector("#content");
+
+   for (let word of words) {
+      let divElement = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.style.display = "none";
+      pElement.textContent = word;
+      divElement.appendChild(pElement);
+      divElement.addEventListener('click', onClick);
+      parentElement.appendChild(divElement);
+
+   };
+
+   function onClick(element) {
+
+      let p = element.target.firstChild;
+      p.style.display = "block";
+
+   };
+
+};
