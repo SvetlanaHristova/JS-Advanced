328b2620700588084ced47b538b9228739257fc5
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-11-10 09:57:12 +0200

    https://judge.softuni.org/Contests/Practice/Index/3519#0

--- JS Advanced - Exam/01. Car Dealers_25 Jun 2022/app.js
+++ JS Advanced - Exam/01. Car Dealers_25 Jun 2022/app.js
@@ -6,7 +6,7 @@ function solve() {
 
   let publishButon = document.getElementById("publish");
   publishButon.addEventListener('click', publish);
-  //document.getElementsByClassName('action-btn edit').forEach(x=>x.addEventListener('click', edit));
+
 
   function publish(e) {
     e.preventDefault();
@@ -33,10 +33,6 @@ function solve() {
 
     document.getElementById('table-body').appendChild(trElement);
     Array.from(document.querySelectorAll('input')).map(x => x.value = '');
-    //let optionsElement = createEl('option');
-    //optionsElement.value = ""
-    //document.getElementById('fuel').prepend(optionsElement);
-    //document.getElementById('fuel').selectedIndex = 0;
     editButton.addEventListener('click', edit);
     sellButton.addEventListener('click', sell);
   }
@@ -48,10 +44,8 @@ function solve() {
     yearElement.value = parentEl.children[2].textContent;
     originalCostElement.value = parentEl.children[4].textContent;
     sellingPriceElement.value = parentEl.children[5].textContent;
-    //let index=
     fuelElement.options[fuelElement.selectedIndex].value = parentEl.children[3].textContent;
-    //document.getElementById('fuel').selectedIndex = info.fuelIndex + 1;
-    document.getElementsByClassName('row')[0].remove();
+    parentEl.remove();
   }
   function sell(e) {
     let parent = e.currentTarget.parentElement.parentElement;
@@ -60,24 +54,24 @@ function solve() {
     let year = parent.children[2].textContent;
     let selling = parent.children[5].textContent;
     let original = parent.children[4].textContent;
-    let diff = selling - original
+    let diff = selling - original;
     let parentElement = document.getElementById('cars-list');
     let liElement = createEl('li', '', 'each-list');
     liElement.appendChild(createEl('span', `${make} ${model}`));
     liElement.appendChild(createEl('span', `${year}`));
-    liElement.appendChild(createEl('span', `${diff.toFixed(2)}`));
+    liElement.appendChild(createEl('span', `${diff}`));
     parentElement.appendChild(liElement);
-    document.getElementsByClassName('row')[0].remove();
+    parent.remove();
     document.getElementById('profit').textContent = (Number(document.getElementById('profit').textContent) + diff).toFixed(2);
-    //let removeEl = document.getElementById('fuel').firstChild;
-    //removeEl.remove();
-    //document.getElementById('fuel').selectedIndex = 0;
   }
+
   function createEl(type, content, classContent) {
     let element = document.createElement(type);
+
     if (content) {
       element.textContent = content;
     }
+    
     if (classContent) {
       element.className = classContent;
     }
