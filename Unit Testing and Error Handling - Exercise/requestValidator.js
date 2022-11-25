4478bb8eb9cf41592fc0eed60433baf768b9714f
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-11-25 11:13:42 +0200

    https://judge.softuni.org/Contests/Practice/Index/2767#0

--- Unit Testing and Error Handling - Exercise/requestValidator.js
+++ Unit Testing and Error Handling - Exercise/requestValidator.js
@@ -1,19 +1,19 @@
 function requestValidator(obj) {
     let methodValid = ["GET", 'POST', 'DELETE', 'CONNECT'];
     let versionValid = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
-    let uriPattern = /[a-z0-9.]+/;
-    let msPattern = /[^<>\&'"]+/;
+    let uriPattern = /^[A-Za-z0-9.]+$/;
+    let msPattern = /[<>\\&'"]+/;
 
-    if (!(obj.method&&methodValid.includes(obj.method))) {
+    if (!(obj.method && methodValid.includes(obj.method))) {
         throw new Error('Invalid request header: Invalid Method');
     }
-    if (!(obj.version&&versionValid.includes(obj.version))) {
-        throw new Error('Invalid request header: Invalid Version');
-    }
-    if (!(obj.uri&&uriPattern.test(obj.uri))){
+    if (!(obj.uri == '*' || obj.uri && uriPattern.test(obj.uri)) || obj.uri == '') {
         throw new Error('Invalid request header: Invalid URI');
     }
-    if (!(obj.message == "" || msPattern.test(obj.message))) {
+    if (!(obj.version && versionValid.includes(obj.version))) {
+        throw new Error('Invalid request header: Invalid Version');
+    }
+    if (!((obj.message == "" || obj.message && !msPattern.test(obj.message)))) {
         throw new Error('Invalid request header: Invalid Message');
     }
     return obj;
@@ -21,12 +21,11 @@ function requestValidator(obj) {
 
 console.log(requestValidator(
     {
-        method: 'GET',
-        uri: 'svn.public.catalog',
-        version: 'HTTP/1.1',
-        message: ''
+        method: 'POST',
+        uri: 'home.bash',
+        version: 'HTTP/2.0',
+        message: 'rm -rf \\*'
     }));
-
 console.log(requestValidator(
     {
         method: 'OPTIONS',
