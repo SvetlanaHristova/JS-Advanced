38a291b9269b81177f14abf9827cfe65cb4f9d3e
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-11-29 13:30:56 +0200

    https://judge.softuni.org/Contests/Practice/Index/2769#3

---
+++ Classes and Attributes - Exercise/sortedList.js
@@ -0,0 +1,31 @@
+class List {
+    constructor() {
+        this.colection = [];
+        this.size = this.colection.length;
+    }
+    add(element) {
+        this.colection.push(element);
+        this.colection.sort((a, b) => a - b);
+        this.size = this.colection.length;
+    }
+    remove(index) {
+        if (index >= 0 && index < this.colection.length) {
+            this.colection.splice(index, 1);
+            this.size = this.colection.length;
+        }
+    }
+    get(index) {
+        if (index >= 0 && index < this.colection.length) {
+            return this.colection[index];
+        }
+    }
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+
+list.remove(1);
+console.log(list.size);
+console.log(list.get(1)); 
