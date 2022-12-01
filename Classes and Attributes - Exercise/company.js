f1496c430fddf62457c3e68b90a6e350b22f9d13
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-12-01 13:57:02 +0200

    https://judge.softuni.org/Contests/Practice/Index/2769#5

--- Classes and Attributes - Exercise/company.js
+++ Classes and Attributes - Exercise/company.js
@@ -8,34 +8,52 @@ class Company {
         } else {
             this.departments.hasOwnProperty(department) ?
                 this.departments[department].push([name, salary, position])
-                : this.departments[department] = [];
+                : this.departments[department] = [[name, salary, position]];
             return `New employee is hired. Name: ${name}. Position: ${position}`;
         }
     }
     bestDepartment() {
-        let array = Object.entries(this.departments);
-        let bestTotalSalary = 0;
+
         let bestDepartment = "";
-        for (let arr of array) {
-            let curenttDepartment = arr.shift();
-            let totalSalary = arrmap(x=>x=x[1]).reduce((a, b) => { a + b });
-            if (bestTotalSalary < totalSalary) {
-                bestTotalSalary = totalSalary;
-                bestDepartment = curenttDepartment;
+        let bestSalary = 0;
+        let avSalary = 0;
+        for (let element in this.departments) {
+            let curentDepartment = element;
+            let totalSalary = 0;
+            let count=0;
+            for (let el of this.departments[element]) {
+                totalSalary += el[1];
+                count++;
+            }
+            let curentavSalary =(totalSalary / count).toFixed(2);
+            if (avSalary < curentavSalary) {
+                avSalary = curentavSalary;
+                bestSalary = totalSalary;
+                bestDepartment = curentDepartment;
             }
         }
-        let a = this.departments[bestDepartment]
-        a.sort((a, b) => {
-            return (b[1] - a[1]) || (a[0]).localeIfmpare(b[0]);
+        this.departments[bestDepartment].sort(([aA, aB, aC], [bA, bB, bC]) => {
+            return bB - aB || (aA).localeCompare(bA);
         });
-        return `Best Department is: ${bestDepartment}\n
-        Average salary: ${(totalSalary / this.departments[bestDepartment].length).toFixed(2)}\n
-        ${this.departments[bestDepartment].forEach(x => { x.join(' ') })}`
-
+        let employees = "";
+        for (let em of this.departments[bestDepartment]) {
+            let str = em.join(' ')
+            employees += str + '\n'
+        }
+        return `Best Department is: ${bestDepartment}\nAverage salary: ${avSalary}\n${employees.trim()}`;
     }
 }
-let c = new Company();
+/*let c = new Company();
 c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());*/
+
+let c = new Company();
 c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
 c.addEmployee("Slavi", 500, "dyer", "Construction");
 c.addEmployee("Stan", 2000, "architect", "Construction");
