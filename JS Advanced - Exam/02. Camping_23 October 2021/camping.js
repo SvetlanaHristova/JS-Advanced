3bc8f0eae4f8da96833ad8938553cc0f1672b860
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-12-04 22:20:19 +0200

    +

---
+++ JS Advanced - Exam/02. Camping_23 October 2021/camping.js
@@ -0,0 +1,36 @@
+class SummerCamp {
+    constructor(organizer, location) {
+        this.organizer = organizer;
+        this.location = location;
+        this.priceForTheCamp = { "child": 150, "student": 300, "collegian": 500 };
+        this.listOfParticipants = [];
+    }
+
+    registerParticipant(name, condition, money) {
+
+        if (!this.priceForTheCamp[participant] || this.priceForTheCamp[participant] != money) {
+
+        }
+
+
+    }
+
+    unregisterParticipant(name) {
+
+    }
+
+    timeToPlay(typeOfGame, participant1, participant2) {
+
+    }
+
+    toString() {
+
+    }
+
+}
+
+const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
